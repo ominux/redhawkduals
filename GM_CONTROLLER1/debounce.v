d2bde08b327fc5493fdeb86bb21808ecc3fd1599
jamieson.peter <jamieson.peter@fa354620-318b-59bd-0e57-2b01a07ce14c>
2010-06-27 21:39:24 +0000

    Added documentation

--- GM_CONTROLLER1/debounce.v
+++ GM_CONTROLLER1/debounce.v
@@ -1,4 +1,30 @@
+/*
 
+This software/hardware design was originally created by Peter Jamieson
+jamiespa@muohio.edu  http://www.users.muohio.edu/jamiespa/
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+*/ 
 module debounce_DE2_SW (clk, rst_n, SW, SWO);
 input rst_n, clk;
 input [17:0]SW;
